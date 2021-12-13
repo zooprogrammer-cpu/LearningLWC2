3e2550d0ab4b536f5b080aa102f7cfb4c71e7dce
zooprogrammer-cpu <60715599+zooprogrammer-cpu@users.noreply.github.com>
2021-12-13 09:36:31 -0600

    querySelector with list of names
    Prints list of names from an array

--- force-app/main/default/lwc/helloQuerySelectorComponent/helloQuerySelectorComponent.js
+++ force-app/main/default/lwc/helloQuerySelectorComponent/helloQuerySelectorComponent.js
@@ -1,9 +1,14 @@
 import { LightningElement } from 'lwc';
 
 export default class HelloQuerySelectorComponent extends LightningElement {
+    userName =["John","Smith","Nikki","Tom" ]
     fetchDetailHandler(){
         const elem=this.template.querySelector('h1')
+        const userElements =this.template.querySelectorAll('.name')
         console.log(elem.innerText)
+        Array.from(userElements).forEach(item=>{
+            console.log(item.innerText)
+        })
 
     }
 }
