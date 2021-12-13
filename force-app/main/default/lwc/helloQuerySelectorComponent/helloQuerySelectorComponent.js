e3e9803cc1719d68866856dbaeab95d3774b2b68
zooprogrammer-cpu <60715599+zooprogrammer-cpu@users.noreply.github.com>
2021-12-13 09:41:51 -0600

    querySelector applies border when button is clicked

--- force-app/main/default/lwc/helloQuerySelectorComponent/helloQuerySelectorComponent.js
+++ force-app/main/default/lwc/helloQuerySelectorComponent/helloQuerySelectorComponent.js
@@ -4,6 +4,9 @@ export default class HelloQuerySelectorComponent extends LightningElement {
     userName =["John","Smith","Nikki","Tom" ]
     fetchDetailHandler(){
         const elem=this.template.querySelector('h1')
+        elem.style.border="1px solid red";
+        console.log(item.innerText)
+
         const userElements =this.template.querySelectorAll('.name')
         console.log(elem.innerText)
         Array.from(userElements).forEach(item=>{
