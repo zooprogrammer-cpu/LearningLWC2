a1fcb5bfd8eac0c076e8cfadf66235e09287c5d5
zooprogrammer-cpu <60715599+zooprogrammer-cpu@users.noreply.github.com>
2021-12-26 12:35:36 -0600

    parent to child pass number and boolean primitive datatypes

--- force-app/main/default/lwc/p2cAlertComponent/p2cAlertComponent.js
+++ force-app/main/default/lwc/p2cAlertComponent/p2cAlertComponent.js
@@ -3,4 +3,6 @@ import { LightningElement,api } from 'lwc';
 export default class P2cAlertComponent extends LightningElement {
     @api message
     @api cardHeading
+    @api number
+    @api isValid
 }
