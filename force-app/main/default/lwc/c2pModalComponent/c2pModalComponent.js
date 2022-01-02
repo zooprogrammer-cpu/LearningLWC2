1f31816a9e8a8550813a8bc5bc0eef98cbe9acde
zooprogrammer-cpu <60715599+zooprogrammer-cpu@users.noreply.github.com>
2022-01-02 14:33:17 -0600

    child to parent with data
    Display data such as a string in the parent with information from child

--- force-app/main/default/lwc/c2pModalComponent/c2pModalComponent.js
+++ force-app/main/default/lwc/c2pModalComponent/c2pModalComponent.js
@@ -3,7 +3,9 @@ import { LightningElement } from 'lwc';
 export default class C2pModalComponent extends LightningElement {
 
     closeHandler(){ 
-        const myEvent = new CustomEvent('close')
+        const myEvent = new CustomEvent('close',{
+            detail:"Modal closed succesfully" 
+        })
         this.dispatchEvent(myEvent)
     }
 }
--- force-app/main/default/lwc/c2pParentComponent/c2pParentComponent.js
+++ force-app/main/default/lwc/c2pParentComponent/c2pParentComponent.js
@@ -2,10 +2,12 @@ import { LightningElement } from 'lwc';
 
 export default class C2pParentComponent extends LightningElement {
     showModal = false
+    msg 
     clickHandler(){ 
         this.showModal = true
     }
-    closeHandler(){ 
+    closeHandler(event){ 
+        this.msg=event.detail
         this.showModal = false
     }
 }
