1f4709a0548fffc3fc5d68893f6db0e277b6729b
zooprogrammer-cpu <60715599+zooprogrammer-cpu@users.noreply.github.com>
2022-01-02 14:57:57 -0600

    child to parent with event bubbling
    event bubbling - keeps on going up to parent and run all actions of the parents.

--- force-app/main/default/lwc/c2pModalComponent/c2pModalComponent.js
+++ force-app/main/default/lwc/c2pModalComponent/c2pModalComponent.js
@@ -4,10 +4,15 @@ export default class C2pModalComponent extends LightningElement {
 
     closeHandler(){ 
         const myEvent = new CustomEvent('close',{
+            bubbles:true,
             detail:{
                 msg:"Modal closed succesfully"
             } 
         })
         this.dispatchEvent(myEvent)
+    }
+
+    footerHandler(){
+        console.log("Footer Event Called")
     }
 }
