bd86f1b7157a4d6537288df91186c798d3a6a3e8
zooprogrammer-cpu <60715599+zooprogrammer-cpu@users.noreply.github.com>
2022-01-08 18:04:50 -0600

    LWC to Aura component -Button in LWC to pass a msg to Aura

--- force-app/main/default/lwc/lwcAuraCommunication/lwcAuraCommunication.js
+++ force-app/main/default/lwc/lwcAuraCommunication/lwcAuraCommunication.js
@@ -1,3 +1,13 @@
-import { LightningElement } from 'lwc';
+import { LightningElement,api } from 'lwc';
 
-export default class LwcAuraCommunication extends LightningElement {}
+export default class LwcAuraCommunication extends LightningElement {
+    @api title
+    callAura(){
+        const event = new CustomEvent('sendmsg',{
+            detail: {
+                "msg":"Hello from LWC"
+            }
+        })
+        this.dispatchEvent(event)
+    }
+}
