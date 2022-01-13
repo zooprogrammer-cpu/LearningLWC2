3d77d86657f984f6bb3b5f2a131366e5fd29fc54
zooprogrammer-cpu <60715599+zooprogrammer-cpu@users.noreply.github.com>
2022-01-13 15:50:37 -0600

    Navigate to recordId in View Mode

---
+++ force-app/main/default/lwc/navigateToRecordPage/navigateToRecordPage.js
@@ -0,0 +1,16 @@
+import { LightningElement } from 'lwc';
+import { NavigationMixin } from 'lightning/navigation'
+
+
+export default class NavigateToRecordPage extends NavigationMixin(LightningElement) {
+    recordViewMode(){
+        this[NavigationMixin.Navigate]({
+            type: 'standard__recordPage',
+            attributes:{
+                recordId:'0035f00000C8oyVAAR',
+                objectApiName:'Contact',
+                actionName:'view'
+            }
+        })
+    }
+}
