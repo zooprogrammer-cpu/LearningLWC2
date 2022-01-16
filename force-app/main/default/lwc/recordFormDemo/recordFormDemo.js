29708e6af89957d1b838c3bcd04ddaf9acb1919e
zooprogrammer-cpu <60715599+zooprogrammer-cpu@users.noreply.github.com>
2022-01-16 14:37:59 -0600

    lightning-record-form - Edit or View Account

--- force-app/main/default/lwc/recordFormDemo/recordFormDemo.js
+++ force-app/main/default/lwc/recordFormDemo/recordFormDemo.js
@@ -1,4 +1,4 @@
-import { LightningElement } from 'lwc';
+import { LightningElement,api } from 'lwc';
 import {ShowToastEvent} from 'lightning/platformShowToastEvent';
 import ACCOUNT_OBJECT from '@salesforce/schema/Account'
 import NAME_FIELD from '@salesforce/schema/Account.Name'
@@ -7,9 +7,10 @@ import TYPE_FIELD from '@salesforce/schema/Account.Type'
 import INDUSTRY_FIELD from '@salesforce/schema/Account.Industry'
 
 export default class RecordFormDemo extends LightningElement {
+    @api recordId
+    @api objectApiName
     objectName = ACCOUNT_OBJECT
     fieldList =[NAME_FIELD,ANNUAL_REVENUE_FIELD,TYPE_FIELD,INDUSTRY_FIELD]
-
     successHandler(event){
         console.log(event.detail.id)
         const toastEvent = new ShowToastEvent({
