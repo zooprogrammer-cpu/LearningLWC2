dedc440c36940cc89133fbeb8d54ab67f66d9aff
zooprogrammer-cpu <60715599+zooprogrammer-cpu@users.noreply.github.com>
2022-01-17 20:00:43 -0600

    Toast messages with reusable method

--- force-app/main/default/lwc/toastNotification/toastNotification.js
+++ force-app/main/default/lwc/toastNotification/toastNotification.js
@@ -3,22 +3,27 @@ import {ShowToastEvent} from 'lightning/platformShowToastEvent';
 
 export default class ToastNotification extends LightningElement {
     toastHandler(){
-        const evt = new ShowToastEvent({
-            title: "Success",
-            message:"Account Created",
-            variant:"success"
-        })
-        this.dispatchEvent(evt)
+        this.showToast("Success","Account Created","success")
     }
 
     toastHandlerError(){
+       this.showToast("Error", "Cannot compute", "error")
+    }
+
+    toastHandlerWarn(){
+        this.showToast("Warn", "Danger Will Robinson", "warning")
+     }
+
+     toastHandlerInfo(){
+        this.showToast("Info", "Here is your information", "info")
+     }
+
+    showToast(title,message,variant){
         const evt = new ShowToastEvent({
-            title: "Error",
-            message: "Danger Will Robinson",
-            variant: "error"
+            title: title,
+            message: message,
+            variant: variant
         })
         this.dispatchEvent(evt)
     }
-
-    
 }
