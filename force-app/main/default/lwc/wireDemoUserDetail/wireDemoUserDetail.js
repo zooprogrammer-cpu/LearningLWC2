de08d18ffe736d0e214f0dca44e276caf2d79d40
zooprogrammer-cpu <60715599+zooprogrammer-cpu@users.noreply.github.com>
2022-01-19 14:32:24 -0600

    wire service method with References for fields

--- force-app/main/default/lwc/wireDemoUserDetail/wireDemoUserDetail.js
+++ force-app/main/default/lwc/wireDemoUserDetail/wireDemoUserDetail.js
@@ -1,12 +1,15 @@
 import { LightningElement, wire } from 'lwc';
 import {getRecord} from 'lightning/uiRecordApi'
 import Id from '@salesforce/user/Id'
+import NAME_FIELD from '@salesforce/schema/User.Name'
+import EMAIL_FIELD from '@salesforce/schema/User.Email'
+const fields=[NAME_FIELD,EMAIL_FIELD]
 
 export default class WireDemoUserDetail extends LightningElement {
     userId = Id
 
     userDetail
-    @wire(getRecord, {recordId:'0055f000003n3UJ', fields:['User.Name','User.Email']})
+    @wire(getRecord, {recordId:'0055f000003n3UJ', fields:fields})
     userDetailHandler({data,error}){
       if(data){
         this.userDetail=data.fields
@@ -16,7 +19,7 @@ export default class WireDemoUserDetail extends LightningElement {
       }
     }
 
-    @wire(getRecord, {recordId:'0055f000003n3UJ', fields:['User.Name','User.Email']})
+    @wire(getRecord, {recordId:'0055f000003n3UJ', fields:fields})
     userDetailProperty
 
 
