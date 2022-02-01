5907444d44a9e3af6a3eeb211c1df82d59b8feae
zooprogrammer-cpu <60715599+zooprogrammer-cpu@users.noreply.github.com>
2022-02-01 17:19:28 -0600

    apexImperativeWithParams Demo with setTimeout

--- force-app/main/default/lwc/apexImperativeWithParamsDemo/apexImperativeWithParamsDemo.js
+++ force-app/main/default/lwc/apexImperativeWithParamsDemo/apexImperativeWithParamsDemo.js
@@ -3,8 +3,16 @@ import findAccounts from '@salesforce/apex/AccountController.findAccounts'
 export default class ApexImperativeWithParamsDemo extends LightningElement {
     searchKey=''
     accounts
+    timer
     searchHandler(event){
+        window.clearTimeout(this.timer)
         this.searchKey = event.target.value
+        this.timer = setTimeout(()=>{
+            this.callApex()
+        },1000)    
+    }
+
+    callApex(){
         findAccounts({searchKey:this.searchKey})
         .then(result=>{
             this.accounts = result
