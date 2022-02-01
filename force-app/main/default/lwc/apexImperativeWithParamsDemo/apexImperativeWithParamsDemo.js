635b6dbedc53f1b9ea9382ddb8b87e49be1df178
zooprogrammer-cpu <60715599+zooprogrammer-cpu@users.noreply.github.com>
2022-02-01 17:07:09 -0600

    apexImperativeWithParams
    Search box for accounts but makes apex call with each letter typed.

---
+++ force-app/main/default/lwc/apexImperativeWithParamsDemo/apexImperativeWithParamsDemo.js
@@ -0,0 +1,15 @@
+import { LightningElement } from 'lwc';
+import findAccounts from '@salesforce/apex/AccountController.findAccounts'
+export default class ApexImperativeWithParamsDemo extends LightningElement {
+    searchKey=''
+    accounts
+    searchHandler(event){
+        this.searchKey = event.target.value
+        findAccounts({searchKey:this.searchKey})
+        .then(result=>{
+            this.accounts = result
+        }).catch(error=>{
+            console.error(error)
+        })
+    }
+}
