c35b5f8e5e9bb67a11965f8f051f3833cd754865
zooprogrammer-cpu <60715599+zooprogrammer-cpu@users.noreply.github.com>
2022-02-06 14:09:53 -0600

    pdfGenerator

--- force-app/main/default/lwc/pdfGenerator/pdfGenerator.js
+++ force-app/main/default/lwc/pdfGenerator/pdfGenerator.js
@@ -1,37 +1,41 @@
-import { LightningElement } from 'lwc';
-
-export default class PdfGenerator extends LightningElement {
-    imageURL='https://www.sparksuite.com/images/logo.png'
+import { LightningElement, api } from 'lwc';
+import generatePDF from '@salesforce/apex/pdfController.generatePDF'
+export default class PdfGenerationDemo extends LightningElement {
+    @api recordId
+    imageUrl = 'https://www.sparksuite.com/images/logo.png'
     invoiceData={
-        invoiceNo: '1234',
-        invoiceCreated: 'January 1, 2020',
-        invoiceDue: 'January 31, 2020',
-        companyName: 'Ash and Co',
-        address1: '1234 High Road',
-        address2: 'Madison, WI'
+        invoiceNo:'123',
+        invoiceCreated:'January 1, 2019',
+        invoiceDue:'January 10, 2020',
+        companyName:'Sparksuite, Inc.',
+        address1:'12345 Sunny Road',
+        address2:' Sunnyville, CA 12345'
     }
-
     clientData={
-        client:"Acme Corp",
-        username:"John Doe",
+        client:'Acme Corp',
+        username:'John Doe',
         email:'john@example.com'
     }
-
     services=[
-        {name:'Consultant fee', amount: 1000.00},
-        {name:'Website design', amount: 300.00},
-        {name:'Hosting (3 Months)', amount: 75.00},
+        {name:'Consultant fee', amount:1000.00},
+        {name:'Website design', amount:300.00},
+        {name:'Hosting (3 months)', amount:75.00}
     ]
-    
+
     get totalAmount(){
-        return this.services.reduce((total,service)=>{
-            return total = total + service.amount
-        },0) 
+        return this.services.reduce((total, service)=>{
+            return total = total+service.amount
+        }, 0)
     }
 
     pdfHandler(){
         let content = this.template.querySelector('.container')
         console.log(content.outerHTML)
+        generatePDF({ recordId:this.recordId, htmlData:content.outerHTML}).then(result=>{
+            console.log("attachment id", result)
+            window.open(`https://ashbaslwc-dev-ed.lightning.force.com//servlet/servlet.FileDownload?file=${result.Id}`)
+        }).catch(error=>{
+            console.error(error)
+        })
     }
-
 }
