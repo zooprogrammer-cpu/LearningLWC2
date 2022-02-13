aeea2f685020706c8a730a8e27ec5773326def3b
zooprogrammer-cpu <60715599+zooprogrammer-cpu@users.noreply.github.com>
2022-02-13 08:46:37 -0600

    filteringAndSorting initial table setup

--- force-app/main/default/lwc/filteringAndSorting/filteringAndSorting.js
+++ force-app/main/default/lwc/filteringAndSorting/filteringAndSorting.js
@@ -1,10 +1,15 @@
 import { LightningElement,wire } from 'lwc';
 import getContactList from '@salesforce/apex/ContactController.getContactList'
 export default class FilteringAndSorting extends LightningElement {
+    headings=["Id", "Name", "Title", "Email"]
+    fullTableData = [] // store the data in thie empty array
+    filteredData = []
     @wire(getContactList)
     contactHandler({data,error}){
         if (data){
             console.log(data)
+            this.fullTableData = data
+            this.filteredData = data
         }
         if (error){
             console.error(error)
