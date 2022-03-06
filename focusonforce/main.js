580361e7266fa83ca698ce7bd8977a8fce80c5e2
zooprogrammer-cpu <60715599+zooprogrammer-cpu@users.noreply.github.com>
2022-03-06 17:10:13 -0600

    renaming imports and exports

--- focusonforce/main.js
+++ focusonforce/main.js
@@ -1,6 +1,6 @@
-import {multiply,divide,SERVER_IP} from './modules/modules.js'
+import {multiplynow as multiplyFile,dividenow as divideFile,SERVER_IP} from './modules/modules.js'
 let a = 5;
 let b = 7;
-console.log(`Multiplication:`,multiply(a,b))
-console.log(`Division:`, parseFloat(divide(a,b).toFixed(2)))
+console.log(`Multiplication:`,multiplyFile(a,b))
+console.log(`Division:`, parseFloat(divideFile(a,b).toFixed(2)))
 console.log(`The server IP is:`, SERVER_IP)
--- focusonforce/modules/modules.js
+++ focusonforce/modules/modules.js
@@ -8,4 +8,4 @@ function divide(num1,num2){
 
 const SERVER_IP = "35.56.98.2"
 
-export {multiply,divide,SERVER_IP};
+export {multiply as multiplynow ,divide as dividenow,SERVER_IP};
