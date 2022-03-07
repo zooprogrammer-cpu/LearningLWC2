d2a945b4f100ef883248dc97274a272812505316
zooprogrammer-cpu <60715599+zooprogrammer-cpu@users.noreply.github.com>
2022-03-06 22:51:04 -0600

    Decorators Example1

--- focusonforce/main.js
+++ focusonforce/main.js
@@ -1,6 +1,13 @@
-import {multiplynow as multiplyFile,dividenow as divideFile,SERVER_IP} from './modules/modules.js'
-let a = 5;
-let b = 7;
-console.log(`Multiplication:`,multiplyFile(a,b))
-console.log(`Division:`, parseFloat(divideFile(a,b).toFixed(2)))
-console.log(`The server IP is:`, SERVER_IP)
+function displayName(name){
+    console.log(name);
+}
+
+function displayNameDecorator(fn){
+    return function(name){
+        const str = `Welcome to the hotel, ${name}`;
+        fn (str);
+    }
+}
+
+const customerName = displayNameDecorator(displayName);
+customerName(`John Wayne`);
--- focusonforce/modulescenario.js
+++ focusonforce/modulescenario.js
@@ -5,9 +5,11 @@ const btn = document.getElementById('btn-print');
 btn.addEventListener('click', loadModulePrint);
 function loadModulePrint() { 
     let num; //initialize num as integer
-    import ('./print.js')
-    .then((module)=>{
-        module.add(num)
-    })
+    // import ('./print.js')
+    // .then((module)=>{
+    //     module.add(num)
+    // })
+    let module = await import("./print.js");
+    module.add(num);
 }
     
