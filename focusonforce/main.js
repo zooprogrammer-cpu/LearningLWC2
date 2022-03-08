e61db8721c5b39d191d1b51cf30bc531a32db082
zooprogrammer-cpu <60715599+zooprogrammer-cpu@users.noreply.github.com>
2022-03-08 09:38:24 -0600

    Bubble and Capture

--- focusonforce/main.js
+++ focusonforce/main.js
@@ -1,14 +1,11 @@
-// //when onchange event occurs (the cursor leaves the password field), user and password are stored in variables
-// function checkPassword(){
-//     let pass = document.getElementById('pass').value;
-//     let user = document.getElementById('user').value
-//     console.log(user,pass)
-// }
+//This has an event handler for each div
+// The useCapture parameter is set to false 
 
-// oneventhandler direclty in javascript
 
-document.getElementById('pass').onchange = function(event){
-    let pass = document.getElementById('pass').value;
-    let user = document.getElementById('user').value;
-    console.log(user,pass);
-}
+document.getElementById('outer').addEventListener('click',function(e){
+    console.log('Click event received by outer div')
+},true);
+
+document.getElementById('inner').addEventListener('click',function(e){
+    console.log('Click event received by inner div')
+},true)
