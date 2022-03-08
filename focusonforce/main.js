87ace90bfbc2ca3b83710b68ca0d675762c73817
zooprogrammer-cpu <60715599+zooprogrammer-cpu@users.noreply.github.com>
2022-03-08 11:00:29 -0600

    preventDefault() =prevent non-alphabets from being typed

--- focusonforce/main.js
+++ focusonforce/main.js
@@ -1,12 +1,27 @@
-//This has an event handler for each div
-// The useCapture parameter is set to false 
+//The outer div has an event listner for keypress event
+//grabbing the e object and grabbing the key property
+//from keypress event
+document.getElementById('outer').addEventListener('keypress',function(e){
+    console.log('Outer div received a keypress:',e.key)
+});
 
+//checkName function- 
+//Prevent anything but letters from showing 
+//using preventDefault while allowing charCode 8 (backspace)
+//in case the user needs to delete a chracter
+function checkName(e){
+    let char =e.key;
+    let charCode=e.charCode;
+    let alphabet ='abcdefghijklmnopqrstuvwxyz';
 
-document.getElementById('outer').addEventListener('click',function(e){
-    console.log('Click event received by outer div')
-},false);
+    // if not a backspace or not a letter, 
+    //we call preventDefault to prevent putting it in that field
+    if (charCode != 8 && !alphabet.includes(char.toLowerCase())){
+    e.preventDefault()
+}
+}
 
-document.getElementById('inner').addEventListener('click',function(e){
-    e.stopPropagation();
-    console.log('Click event received by inner div')
-},false)
+//grab the field here and add the addEventListener 
+//for the keypress event and we call the function checkName
+const nameField = document.getElementById('name');
+nameField.addEventListener('keypress',checkName);
