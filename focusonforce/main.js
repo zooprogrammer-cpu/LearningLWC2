c66ccacedf3d3786fadd3df476a166196a28f8a7
zooprogrammer-cpu <60715599+zooprogrammer-cpu@users.noreply.github.com>
2022-03-08 09:05:28 -0600

    onevent in HTML and onevent directly in javascript

--- focusonforce/main.js
+++ focusonforce/main.js
@@ -1,43 +1,14 @@
-function loginUser(){
-    console.log('Logging in user')
+// //when onchange event occurs (the cursor leaves the password field), user and password are stored in variables
+// function checkPassword(){
+//     let pass = document.getElementById('pass').value;
+//     let user = document.getElementById('user').value
+//     console.log(user,pass)
+// }
+
+// oneventhandler direclty in javascript
+
+document.getElementById('pass').onchange = function(event){
+    let pass = document.getElementById('pass').value;
+    let user = document.getElementById('user').value;
+    console.log(user,pass);
 }
-
-// Get the login-button lement and add the event listener 
-//using the addEventListener() method
-
-// const loginButton = document.getElementById('login-button');
-// loginButton.addEventListener('click',function(event){
-//     loginUser(event);
-// })
-
-// loginButton.addEventListener('mouseover',function(event){
-//     event.target.style.color ='white';
-// })
-
-// loginButton.addEventListener('mouseleave',function(event){
-//     event.target.style.color='black';
-// })
-
-function onClickItem(event){
-    let text = event.target.textContent; 
-    console.log(`Mouse coordinates:${event.clientX},${event.clientY}`)
-    console.log(`Captured text:${text}`)
-}
-document.querySelector('ol').addEventListener('click',onClickItem)
-
-//Example of an anonymous event listener
-let button = document.querySelector('button');
-button.addEventListener('click',function(e){
-    console.log('Timer starts now...')
-});
-
-//Example of named event listener
-
-function startTimer(){
-    console.log('Timer starts now..')
-}
-button.addEventListener('click',startTimer); 
-
-
-// A named listener allows it to be easily removed using removeEnveListener();
-button.removeEventListener('click',startTimer)
