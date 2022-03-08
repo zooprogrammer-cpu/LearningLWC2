8e0c7e64b675e92805ab2c9b022a73e0e4aa868d
zooprogrammer-cpu <60715599+zooprogrammer-cpu@users.noreply.github.com>
2022-03-07 20:39:32 -0600

    addEventListener and Event

--- focusonforce/main.js
+++ focusonforce/main.js
@@ -1,11 +1,26 @@
-
-var globalKey ="fsdff"
-function decryptDate(data){
-    var key ="adfsdfaeffv&*&#"
-    console.log(key)
-    console.log(globalKey)
+function loginUser(){
+    console.log('Logging in user')
 }
 
-decryptDate()
-console.log(globalKey)
-console.log(key)
+// Get the login-button lement and add the event listener 
+//using the addEventListener() method
+
+// const loginButton = document.getElementById('login-button');
+// loginButton.addEventListener('click',function(event){
+//     loginUser(event);
+// })
+
+// loginButton.addEventListener('mouseover',function(event){
+//     event.target.style.color ='white';
+// })
+
+// loginButton.addEventListener('mouseleave',function(event){
+//     event.target.style.color='black';
+// })
+
+function onClickItem(event){
+    let text = event.target.textContent; 
+    console.log(`Mouse coordinates:${event.clientX},${event.clientY}`)
+    console.log(`Captured text:${text}`)
+}
+document.querySelector('ol').addEventListener('click',onClickItem)
