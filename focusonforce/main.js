32b007229592c9bb93c9c3b6b66bddac3ba992b7
zooprogrammer-cpu <60715599+zooprogrammer-cpu@users.noreply.github.com>
2022-03-09 20:04:28 -0600

    DOM and Window Object

--- focusonforce/main.js
+++ focusonforce/main.js
@@ -1,27 +1,16 @@
-//The outer div has an event listner for keypress event
-//grabbing the e object and grabbing the key property
-//from keypress event
-document.getElementById('outer').addEventListener('keypress',function(e){
-    console.log('Outer div received a keypress:',e.key)
-});
-
-//checkName function- 
-//Prevent anything but letters from showing 
-//using preventDefault while allowing charCode 8 (backspace)
-//in case the user needs to delete a chracter
-function checkName(e){
-    let char =e.key;
-    let charCode=e.charCode;
-    let alphabet ='abcdefghijklmnopqrstuvwxyz';
-
-    // if not a backspace or not a letter, 
-    //we call preventDefault to prevent putting it in that field
-    if (charCode != 8 && !alphabet.includes(char.toLowerCase())){
-    e.preventDefault()
-}
+function setVariables(){
+    let maxVisitors = 1000; //set a local variable
+    window.minVisitors = 25; // set a global variable
 }
 
-//grab the field here and add the addEventListener 
-//for the keypress event and we call the function checkName
-const nameField = document.getElementById('name');
-nameField.addEventListener('keypress',checkName);
+let [userid,timestamp] = [15,1606801541];
+let state ={id: userid, ts:timestamp};
+let url ="?id=" + userid +'&ts=' + timestamp; 
+//push an entry to the browser's history stack
+window.history.pushState(state,'', url);
+
+setVariables();
+console.log('this=== window:', this === window);
+console.log('minVisitors:', minVisitors);
+console.log('this.minVisitors:', this.minVisitors);
+console.log('maxVisitors:', maxVisitors);
