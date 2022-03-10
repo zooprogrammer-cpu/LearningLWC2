94633a2fb33d267bf304f3966bde856d51feb6fc
zooprogrammer-cpu <60715599+zooprogrammer-cpu@users.noreply.github.com>
2022-03-09 21:21:12 -0600

    appendChild() to add element to the end of parent node

--- focusonforce/main.js
+++ focusonforce/main.js
@@ -1,5 +1,5 @@
-const h1 = document.createElement('h1'); 
-h1.innerHTML = 'WEEEEEELLLLCOOOMMMMEEEE'; //content is added to the h1 element
-document.body.insertBefore(h1,document.getElementById('content'));
+const p = document.createElement('p');
+p.innerHTML = 'We hope you learn a lot!';
+document.getElementById('content').appendChild(p);
 
 
