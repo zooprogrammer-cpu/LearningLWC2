34f3a0fdf633b653927d7389f82fbe26f91390ed
zooprogrammer-cpu <60715599+zooprogrammer-cpu@users.noreply.github.com>
2022-03-09 20:44:36 -0600

    querySelector(all) to save into an array

--- focusonforce/main.js
+++ focusonforce/main.js
@@ -1,6 +1,10 @@
+let arr =[]; 
 const paragraphs = document.querySelectorAll('p'); 
 for(let elem of paragraphs){
     elem.style.color ='red';
+    arr.push(elem.innerHTML)
 }
+
+console.log(arr)
 
 
