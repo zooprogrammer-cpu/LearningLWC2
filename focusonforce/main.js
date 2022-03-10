f518cd18a16e3bfa54f70822e06f205f795e8e1c
zooprogrammer-cpu <60715599+zooprogrammer-cpu@users.noreply.github.com>
2022-03-09 20:31:11 -0600

    querySelector

--- focusonforce/main.js
+++ focusonforce/main.js
@@ -1,15 +1,4 @@
-const msg = document.getElementById('message');
-msg.innerHTML = '<span>Good Morning</span>';
-
-//The following properties and attributes are available in the DOM element
-
-console.log(msg.tagName); //tag name of the element
-console.log(msg.innerHTML); //HTML content of the element
-console.log(msg.classList); //class attributes of the element
-
-//retreive the value of a specified attribute
-console.log(msg.getAttribute('id'));
-//retreive the size and position relative to the viewport
-console.log(msg.getBoundingClientRect());
+const msg = document.querySelector('#message'); 
+msg.innerHTML = '<h1> Good Morning</h1>';
 
 
