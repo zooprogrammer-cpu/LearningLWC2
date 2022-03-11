9e0125d637f647d8e57a2a93bc031c83a043522e
zooprogrammer-cpu <60715599+zooprogrammer-cpu@users.noreply.github.com>
2022-03-11 16:10:31 -0600

    History API

--- focusonforce/main.js
+++ focusonforce/main.js
@@ -1,32 +1,26 @@
-let locationElement = document.getElementById('location');//use DOM APi to grab location
-const options ={ //These are options to be passed to the getCurrentPosition() method.
-    enableHighAccuracy :true, //Enable high accuracy.
-    timeout: 5000, //Amount of time before the error callback is invoked.
-    maximumAge: 0 //Maximum cached position age in milliseconds.
-};
-
-// if getCurrentPosition is successful
-const handleSuccess =(position) =>{
-    const result = position.coords; //save the coordinates of the location
-    let message ='You are at:<br/>' +
-    `Latitude: ${result.latitude}<br/>`+
-    `Longitude: ${result.longitude}</br>`+
-    `+/- ${result.accuracy} meters.`;
-    locationElement.innerHTML = message; 
+window.onpopstate= function(event){
+    document.getElementById('message').innerHTML = `You are here:${document.location}, state:${JSON.stringify(event.state)}`;
 }
-// if getCurrentPosition is not successful
-const handleError = (error) =>{
-    console.warn(`ERROR(${error.code}): ${error.message}`);
+const backward=()=>{ //Navigate backward one step in the history stack or use history.go(-1)
+    history.back();
 }
-// this is the method that is called when the get location button is clicked and this 
-//uses the geolocation API
-const getLocation = ()=>{
-    if (navigator.geolocation){
-        navigator.geolocation.getCurrentPosition(handleSuccess,handleError,options);
-    } else{
-        locationElement.innerHTML ='Sorry, this browser does not support Geolocation';
-    }
+const forward =()=>{ //Navigate forward one step in the history stack or use history.go(1)
+    history.forward(); 
 }
 
-
-
+//Replace the top page of history stack with this page by calling the replaceState() method of the hostory object
+const replace=()=>{
+    history.replaceState(
+        {page:3},  //a JavaScript object that represents the state of a page
+        "Page 3",  //the page title
+        "?page=3") //the page URL which replaces the top of the history stack 
+    
+}
+//Push a page to the history stack by calling the pushState() method of the history object
+const push =(state,title,url)=>{
+    history.pushState(
+            state,  //a JavaScript object that represents the state of a page
+            title,  // the page title
+            url);   // the page URL that is added to the history stack
+        
+}
