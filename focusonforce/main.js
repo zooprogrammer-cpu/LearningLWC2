a0d7557aeb4fec07a0e38537a41970346aa49433
zooprogrammer-cpu <60715599+zooprogrammer-cpu@users.noreply.github.com>
2022-03-11 13:05:20 -0600

    DOM API Button for a Counter

--- focusonforce/main.js
+++ focusonforce/main.js
@@ -1,31 +1,11 @@
-function createTable(rows, columns) {
-    const body = document.querySelector('body'), table = document.createElement('table'), tBody = document.createElement('tbody');
-    let count = 0;
-    function createCells(row) {
-        for (let i = 0; i < columns; i++) {
-            const cell = document.createElement('td'); // Create cells for row     
-            count++;     
-            cell.style = 'text-align:center;';     
-            cell.innerHTML = count;     
-            row.appendChild(cell);   
-        } 
-    };
-    
-    function createRows() {
-            for (let i = 0; i < rows; i++) {
-                const row = document.createElement('tr'); // Create row     
-                createCells(row);     
-                tBody.appendChild(row);   
-            } };
+let counter =0;
+const onClick = (event) =>{
+    //Get the HTML element with the Id of 'value'
+    const calue = document.getElementById('value');
+    //Set the HTML markup within the element to an incremented value of 'counter'.
+    value.innerHTML =++counter;
+}
 
-
-    createRows(); 
-    table.appendChild(tBody); // Add table body to table. 
-    body.appendChild(table); // Add table to document body. 
-    table.setAttribute('border', '1'); 
-    table.setAttribute('width', '300'); 
-    table.setAttribute('height', '200');};
-    createTable(4, 3); // Create a table of 4 rows and 3 columns
 
 
 
