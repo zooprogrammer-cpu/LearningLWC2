5f40f26ef5e2760a812fb53a8c66842444bfea72
zooprogrammer-cpu <60715599+zooprogrammer-cpu@users.noreply.github.com>
2022-03-12 12:39:21 -0600

    URL to Object Key Value pair

--- focusonforce/main.js
+++ focusonforce/main.js
@@ -1,27 +1,26 @@
-//When the page loads, this JavaScript code uses getItem() to get the name from the local storage and display it
-if (typeof(Storage) !== "undefined") {
-    const firstName = localStorage.getItem('firstName');
-    const lastName = localStorage.getItem('lastName');
-    if (firstName || lastName) document.getElementById("welcome").innerHTML = `Welcome back, ${firstName ? firstName : ''}${lastName ? ' ' + lastName : ''}!`;
-}
-    
-// This function uses setItem() to store both First Name and Last Name in local storage.
-const save = (event) => {const firstName = document.getElementById('firstName').value;
-const lastName = document.getElementById('lastName').value;
-if (firstName && lastName) {
-    // Call setItem() to store in local storage.        
-    localStorage.setItem('firstName', firstName);
-    localStorage.setItem('lastName', lastName);        
-    document.getElementById("thankyou").innerHTML= `Thank you, ${firstName}${lastName}!`;    
-}
-}
-// This function uses removeItem() to remove the First Name from localStorage.
-const clearFirstName = (event) => {    
-    localStorage.removeItem('firstName');     
-    document.getElementById("firstName").value = '';
-    
-}// This function uses clear() to clear the local storage.
-const clearAll = () => {    
-    localStorage.clear();
-}
 
+// const url = new URL(window.location.href)
+// console.log(url)
+
+// const pathname = url.pathname
+
+// //console.log(`pathname`.pathname)
+
+// console.log(`category => ${pathname.split('/')[1]}`);
+
+const urlstring='http://bookbin.com/programming/search?name=JavaScript&year=2005&condition=used#used'
+const urlobject = new URL(urlstring);
+
+// urlobject.pathname returns a USVString which maps to a String object in JavaScript
+const pathname = urlobject.pathname;
+
+// Split the pathname separated by '/' and display the second half
+console.log(`category => ${pathname.split('/')[1]}`);
+
+// Extract the search parameters and print out each key-value pair// urlobject.searchParams returns a URLSearchParams object// In order to manipulate it as a string, it needs to be converted to one with .toString()
+const paramstring = urlobject.searchParams.toString();
+const paramslist = paramstring.split('&'); // This returns an array of strings previously separated by '&'
+for (item in paramslist) { // Iterate over the array
+    const pairs = paramslist[item].split('=');// Further split the string separated by '=' to return an array of 2 items  
+    console.log(`${pairs[0]} => ${pairs[1]}`);// Display each key-value pair
+}
