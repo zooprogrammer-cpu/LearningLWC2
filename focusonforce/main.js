fd9196239bf8ba4b0026de5fc768c23b5f3adeda
zooprogrammer-cpu <60715599+zooprogrammer-cpu@users.noreply.github.com>
2022-03-16 09:38:04 -0500

    Practice Exam 1 reduce method

--- focusonforce/main.js
+++ focusonforce/main.js
@@ -1,7 +1,12 @@
 let items= [
-    {name: 'Laptop X1000', price:999, quantity:1},
-    {name: 'Laptop X2000', price:799, quantity:2},
-    {name: 'Laptop X3000', price:499, quantity:3},
+    {name: 'Laptop X1000', price:2, quantity:1},
+    {name: 'Laptop X2000', price:5, quantity:2},
+    {name: 'Laptop X3000', price:6, quantity:3},
 
 
 ]
+
+const total = items.reduce((accumulator, value)=>{
+return(accumulator + value.price * value.quantity)},0)
+
+console.log(total)
