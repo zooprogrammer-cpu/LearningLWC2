96cdcc05d7630116c45a81b69eef8a4e04501979
zooprogrammer-cpu <60715599+zooprogrammer-cpu@users.noreply.github.com>
2022-03-19 11:12:51 -0500

    set and map

--- focusonforce/main.js
+++ focusonforce/main.js
@@ -1,5 +1,19 @@
 // Sets and Maps
 
+// Set is created from an array so that only unique values are included. 
 let decades = [1990,1990,2000,2010]
 let uniqueDecades = new Set(decades)
-console.log(uniqueDecades)
+uniqueDecades.add(2005)
+console.log(uniqueDecades)  //outputs Set(4) {1990, 2000, 2010, 2005}
+
+//map is used instead of object to store values 
+let quizScores = new Map()
+quizScores.set('quiz1',[100,200,300,400])
+quizScores.set('quiz2',[234,231,564,789])
+quizScores.set('quiz3',[456,789,012,333])
+
+console.log(quizScores.values())
+//prints
+//MapIterator {Array(4), Array(4), Array(4)}
+
+//console.log(`Quiz1 values are:`,quizScores)
