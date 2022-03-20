37d8eaf1f4f46f00a1c6b92d2b16a5f61bd3952b
zooprogrammer-cpu <60715599+zooprogrammer-cpu@users.noreply.github.com>
2022-03-20 16:21:40 -0500

    promise creation

--- focusonforce/main.js
+++ focusonforce/main.js
@@ -1,29 +1,11 @@
-//synchronous callback
-let numbers =[1,2,3,4,5,6,7,8];
-const num = numbers.filter(item=>item>4);
-console.log(num)
-
-//1. asynchronous callback - callback is a function 
-//passed into another function as an arguement
-function loadData(callback){
-    let xhr = new XMLHttpRequest();
-    xhr.open('GET','https://api.github.com/orgs/nodejs')
-    xhr.onload = function(){
-        callback(xhr.response)
-    }
-    xhr.send()
-}
-
-loadData(function(result){
-    console.log(result)
+//promise creation
+var completed = true //by default we set completed to true
+var task = new Promise(function(resolve,reject){
+    setTimeout(()=>{
+        if (completed){
+            resolve("task completed")
+        } else {
+            reject("task is rejected")
+        }
+    },5000)
 })
-console.log("data is present") // this prints first
-
-//2. generate asynchronocity using setTimeout
-
-setTimeout(function(a,b){
-    console.log(a)
-    console.log(b)
-},5000,"Salesforce", "Hello")
-
-//callback hell
