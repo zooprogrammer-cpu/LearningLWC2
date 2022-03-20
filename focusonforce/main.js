a322190295d3aa77da429524e627bad0fdd3ebe2
zooprogrammer-cpu <60715599+zooprogrammer-cpu@users.noreply.github.com>
2022-03-20 15:06:05 -0500

    blocking function

--- focusonforce/main.js
+++ focusonforce/main.js
@@ -1,7 +1,22 @@
-const xhr = new XMLHttpRequest();
-xhr.open('GET','https://jsonplaceholder.typicode.com/todos/1',true);
-xhr.onload= function(){ //callback function
-    let todo =JSON.parse(xhr.responseText);
-    console.log(todo)
-};
-xhr.send(null)
+// let x =10;
+// function timesTen(a){
+//     return a * 10
+// } 
+
+// let y = timesTen(x);
+// console.log (y)
+
+////// task(message) is a Blocking function that 
+//is taking a long time to execute
+
+function task (message){
+    let n = 100000000;
+    while (n>0){
+        n--;
+    }
+    console.log(message)
+}
+
+console.log(`Start the script..`)
+task (`Download a file`)
+console.log(`Done`)
