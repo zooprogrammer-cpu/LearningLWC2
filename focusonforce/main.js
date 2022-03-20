8674b05f1b671c38f4152c35a587e459ccfc3cfc
zooprogrammer-cpu <60715599+zooprogrammer-cpu@users.noreply.github.com>
2022-03-20 15:49:50 -0500

    Synchronous and asynchronous callbacks examples

--- focusonforce/main.js
+++ focusonforce/main.js
@@ -1,22 +1,29 @@
-// let x =10;
-// function timesTen(a){
-//     return a * 10
-// } 
+//synchronous callback
+let numbers =[1,2,3,4,5,6,7,8];
+const num = numbers.filter(item=>item>4);
+console.log(num)
 
-// let y = timesTen(x);
-// console.log (y)
-
-////// task(message) is a Blocking function that 
-//is taking a long time to execute
-
-function task (message){
-    let n = 100000000;
-    while (n>0){
-        n--;
+//1. asynchronous callback - callback is a function 
+//passed into another function as an arguement
+function loadData(callback){
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET','https://api.github.com/orgs/nodejs')
+    xhr.onload = function(){
+        callback(xhr.response)
     }
-    console.log(message)
+    xhr.send()
 }
 
-console.log(`Start the script..`)
-task (`Download a file`)
-console.log(`Done`)
+loadData(function(result){
+    console.log(result)
+})
+console.log("data is present") // this prints first
+
+//2. generate asynchronocity using setTimeout
+
+setTimeout(function(a,b){
+    console.log(a)
+    console.log(b)
+},5000,"Salesforce", "Hello")
+
+//callback hell
