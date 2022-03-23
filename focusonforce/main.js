5a1adab7585be61da01dd9283e317f1176d54d6d
zooprogrammer-cpu <60715599+zooprogrammer-cpu@users.noreply.github.com>
2022-03-22 21:58:00 -0500

    catch to hold the error for then and promise

--- focusonforce/main.js
+++ focusonforce/main.js
@@ -9,7 +9,7 @@ let p = new Promise((resolve,reject)=>{
 p.then((result)=>{
     console.log(result);
     return result * 2;
-}).then((result)=>{
-    console.log(result);
-    return result * 3;
-});
+}).then((result1)=>{ //the previous promise is capture by then
+    console.log(result1);
+    throw new Error("Something is Wrong")
+}).catch (error=>console.error(error))//catch to hold the error for then and promise 
