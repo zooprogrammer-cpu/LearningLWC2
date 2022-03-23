d40d695575ea8df8c53a1826a5ff2463d54584b5
zooprogrammer-cpu <60715599+zooprogrammer-cpu@users.noreply.github.com>
2022-03-23 15:15:51 -0500

    Promise return with async keyword

--- focusonforce/main.js
+++ focusonforce/main.js
@@ -1,13 +1,5 @@
-var job1 = new Promise((resolve,reject)=> setTimeout(resolve,2000,'Job1 ready'))
-var job2 = new Promise((resolve,reject)=> setTimeout(reject,1000,'Job2 ready'))
-var job3 = new Promise((resolve,reject)=> setTimeout(resolve,3000,'Job3 ready'))
-
-
-// Promise.all([job1,job2,job3]).then(results=>{
-//     console.log("SUCCESSFUL",results)
-// })
-
-Promise.race([job1,job2,job3]).then(results=>console.log("result is here",results))
-.catch(err=>{
-    console.log("err",err)
-})
+//async and await
+async function abc(){
+    return "hello"
+}
+abc().then(result=>console.log(result))
