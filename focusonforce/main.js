fdb25d4f3dd753c18d17dd6e50504cd2b37746f7
zooprogrammer-cpu <60715599+zooprogrammer-cpu@users.noreply.github.com>
2022-03-23 15:37:32 -0500

    await

--- focusonforce/main.js
+++ focusonforce/main.js
@@ -1,5 +1,9 @@
 //async and await
 async function abc(){
-    return "hello"
+    let promise = new Promise((resolve,reject)=>{
+        setTimeout(()=> resolve("Hello!"),3000)
+    })
+    let greeting = await promise
+    return greeting
 }
 abc().then(result=>console.log(result))
