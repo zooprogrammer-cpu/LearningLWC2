b801777a107749890b78ffa2c67acbac584a6299
zooprogrammer-cpu <60715599+zooprogrammer-cpu@users.noreply.github.com>
2022-03-24 13:28:03 -0500

    console.time with external callApi

--- focusonforce/main.js
+++ focusonforce/main.js
@@ -1,7 +1,12 @@
-var x =[
-    {name: "Bob",language:"JavaScript"},
-    {name:"Karkara",language:"Python"}]
-
-console.table(x,"name")
+function callApi(){
+    console.time(`API TIMER`)
+    fetch('https://jsonplaceholder.typicode.com/todos/1')
+        .then(response => response.json())
+        .then(json =>{
+            console.timeEnd('API TIMER')//prints time taken by the API
+            console.table(json) //prints the response of the API
+        })
+}
+callApi()
 
 
