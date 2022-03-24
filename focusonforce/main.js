82fa0cd9aab91bb33960c1c724567482fa87b00a
zooprogrammer-cpu <60715599+zooprogrammer-cpu@users.noreply.github.com>
2022-03-24 13:06:08 -0500

    console.table

--- focusonforce/main.js
+++ focusonforce/main.js
@@ -2,7 +2,6 @@ var x =[
     {name: "Bob",language:"JavaScript"},
     {name:"Karkara",language:"Python"}]
 
-console.log ("Hello, %s. You've called me %d times and object is %o","Tom",10,x)
-
+console.table(x,"name")
 
 
