025d1ded9cd7dbfc6125b719a2b8854084deb9d8
zooprogrammer-cpu <60715599+zooprogrammer-cpu@users.noreply.github.com>
2022-03-24 22:00:52 -0500

    debugger

--- focusonforce/main.js
+++ focusonforce/main.js
@@ -1,12 +1,9 @@
-var x =[
-    {name:"Ash", language:"javaScript", age:37},
-    {name:"Dug", language:"bark", age:4}
-]
-x.forEach(item=>{
-    console.group(item.name)
-    console.log(`${item.name} is ${item.age} old`) 
-    console.log(`${item.name} is a ${item.language} developer`) 
-    console.groupEnd(item.name)
-})
+document.querySelector('#btn').addEventListener('click',(event)=>{
+    console.log('start')
+    console.log(event)
+    debugger
+    console.log('end')
+});
+
 
 
