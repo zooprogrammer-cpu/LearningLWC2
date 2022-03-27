20c6c29b8e00523ff9823c23af8b32c2cf93eee1
zooprogrammer-cpu <60715599+zooprogrammer-cpu@users.noreply.github.com>
2022-03-27 09:57:27 -0500

    every method

--- focusonforce/main.js
+++ focusonforce/main.js
@@ -1,37 +1,14 @@
-//map method
-//map - heavily used in LWC. It takes a callback. 
-//callback is a function that passes an arguement to antoher function
-//helpful when creating a checkbox or picklist
-
+// every method
+//return true or false if all elements satisfy condition
 var people = [
     {name:"ash", age: 37},
     {name:"brita", age: 33},
     {name:"dug",age:4}
 ]
 
-console.log(people)
-//0: {name: 'ash', age: 37}
-//1: {name: 'brita', age: 33}
-//2: {name: 'dug', age: 4}
-
-newPeople = people.map(function(currItem,index,actualArr){
-    console.log("currItem",currItem);
-    console.log("index",index);
-    console.log("actualArr",actualArr);
-    //currItem {name:'ash', age:37}
-    //index 0 
-    //actualArr 
-    //0: {name: 'ash', age: 37}
-    //1: {name: 'brita', age: 33}
-    //2: {name: 'dug', age: 4}
-
-    return {"key":currItem.name, "value":currItem.age}
+x = people.every(function(currItem, index,actualArr){
+    return currItem.age>8
 
 })
 
-console.log(newPeople)
-//0: {key: 'ash', value: 37}
-//1: {key: 'brita', value: 33}
-//2: {key: 'dug', value: 4}
-
-
+console.log(x) //false
