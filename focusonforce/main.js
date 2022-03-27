7a8d9424e2b13f91db3623fd0a5748af8cccdf50
zooprogrammer-cpu <60715599+zooprogrammer-cpu@users.noreply.github.com>
2022-03-27 12:38:28 -0500

    for of loop

--- focusonforce/main.js
+++ focusonforce/main.js
@@ -1,21 +1,16 @@
-//reduceRight method
+//for of loop lets us break when forEach does not
 
-var numbers =[1,2,3,4,5]
+const alphabets =[
+    {name: "A"},
+    {name: "B"},
+    {name: "C"},
+    {name: "D"}
+]
 
-numbers.reduceRight(function(total,currItem,index,actualArr){
-    console.log("total:", total) 
-    console.log("current value is:", currItem)
-    return total+currItem
-},0)
-
-/*total: 0
-main.js:7 current value is: 5
-main.js:6 total: 5
-main.js:7 current value is: 4
-main.js:6 total: 9
-main.js:7 current value is: 3
-main.js:6 total: 12
-main.js:7 current value is: 2
-main.js:6 total: 14
-main.js:7 current value is: 1
-*/
+for(let alphabet of alphabets){
+    if (alphabet.name === "C"){
+        break
+    }
+    else
+    console.log("Alphabet is",alphabet.name)
+}
