c85a9b4bf7f0b9c25a8abd7a398f0be4f9085cd2
zooprogrammer-cpu <60715599+zooprogrammer-cpu@users.noreply.github.com>
2022-03-27 15:22:55 -0500

    hoisting example

--- focusonforce/main.js
+++ focusonforce/main.js
@@ -1,9 +1,26 @@
-//scope pollution
-let num = 50; 
-function logNum(){
-    num = 100
-    console.log(num)
-}
+// //hoisting example 1
+// var variable =10;
+// function test(){
+//     console.log(variable)
+//     var variable = 20;
+//     console.log(variable)
+// }
 
-logNum() //prints 100
-console.log(num) //prints 100
+// test() //prints 
+//undefined
+//20
+
+//hoisting example 2
+
+var variable = 10;
+function test2(){
+    console.log(variable)
+    variable = 20; 
+    console.log(variable)
+}
+test2()  
+// prints 10
+// prints 20 
+
+
+
