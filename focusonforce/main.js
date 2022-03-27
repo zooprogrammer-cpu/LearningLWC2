b0bb26bfa9aa494054ccbcea11fec959ec462fcd
zooprogrammer-cpu <60715599+zooprogrammer-cpu@users.noreply.github.com>
2022-03-27 08:33:22 -0500

    Arrays

--- focusonforce/main.js
+++ focusonforce/main.js
@@ -1,24 +1,38 @@
-//Arrays
-//1. Array Literal 
-var arr = [1,2,3,4,5]
-var arr2 = ["sun","moon"]
-var mixedArr = [true, 1,"three"]
+//array methods
 
+//splice to remove. Updates exixsting array
 
-//2. Array constructor method
-var stringArray = new Array()
-stringArray[0] = "one"
+x = ['a','b','c','d','e']
+x.splice(2,2)
+console.log(x) // a,b,e
 
-var numericArray = new Array(3)
-console.log(numericArray)//3 blocks available but empty
-console.log(numericArray.length) // prints 3
-numericArray[0] = 9;
-numericArray[1] =8;
-numericArray[2]=2;
-numericArray[3]=6;
-console.log(numericArray)// prints 9,8,2,6
+//use splice to add. Updates exixsting array
+y=['j','k','l','m'] 
+y.splice(1,0,'hurray')
+console.log(y) // (5) ['j', 'hurray', 'k', 'l', 'm']
 
-//Pass value to constructor directly
-var mixedArray = new Array(4,"two",89)
-console.log(mixedArray)
+//indexOf()
+z = ['tom','bob','lucas','bob']
+console.log(z.indexOf('bob')) //1
+
+//lastIndexOf()
+console.log(z.lastIndexOf('bob')) //3
+
+//join array into a string
+console.log(z.join('')) //tomboblucasbob
+
+//join array with a specific seperator
+console.log(z.join('-')) //tom-bob-lucas-bob
+
+//slice returns a new array
+var arr3 = ['4','5','8','2','1']
+var newArr3 = arr3.slice(1,3)
+console.log(newArr3) // ['5', '8']
+
+//concat
+conArr1 = ["This is a"]
+conArr2 =["great day"]
+conArr3=conArr1.concat(conArr2)
+console.log(conArr3)  //(2) ['This is a', 'great day']
+
 
