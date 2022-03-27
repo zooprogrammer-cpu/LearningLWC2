b84ea2391fb39b0e4e28b82d5c6255b5b31fc203
zooprogrammer-cpu <60715599+zooprogrammer-cpu@users.noreply.github.com>
2022-03-27 10:43:10 -0500

    reduceRight method

--- focusonforce/main.js
+++ focusonforce/main.js
@@ -1,22 +1,21 @@
-//reduce method
+//reduceRight method
 
 var numbers =[1,2,3,4,5]
 
-numbers.reduce(function(total,currItem,index,actualArr){
+numbers.reduceRight(function(total,currItem,index,actualArr){
     console.log("total:", total) 
     console.log("current value is:", currItem)
     return total+currItem
 },0)
-/*
 
-main.js:6 total: 0
-main.js:7 current value is: 1
-main.js:6 total: 1
-main.js:7 current value is: 2
-main.js:6 total: 3
-main.js:7 current value is: 3
-main.js:6 total: 6
-main.js:7 current value is: 4
-main.js:6 total: 10
+/*total: 0
 main.js:7 current value is: 5
+main.js:6 total: 5
+main.js:7 current value is: 4
+main.js:6 total: 9
+main.js:7 current value is: 3
+main.js:6 total: 12
+main.js:7 current value is: 2
+main.js:6 total: 14
+main.js:7 current value is: 1
 */
