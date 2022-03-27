1711670914c51c0e7c30f9bc25e4729b0ddc1817
zooprogrammer-cpu <60715599+zooprogrammer-cpu@users.noreply.github.com>
2022-03-27 10:39:26 -0500

    reduce method

--- focusonforce/main.js
+++ focusonforce/main.js
@@ -1,17 +1,22 @@
-// sort method for strings. mutates original array
-var people = [
-    {name:"ash", age: 37},
-    {name:"brita", age: 33},
-    {name:"dug",age:4}
-]
+//reduce method
 
-people.sort(function(a,b){
-    return (a.name>b.name) ? 1:(a.name<b.name)? -1:0
-})
+var numbers =[1,2,3,4,5]
 
-console.log(people)
+numbers.reduce(function(total,currItem,index,actualArr){
+    console.log("total:", total) 
+    console.log("current value is:", currItem)
+    return total+currItem
+},0)
 /*
-0: {name: 'ash', age: 37}
-1: {name: 'brita', age: 33}
-2: {name: 'dug', age: 4}
+
+main.js:6 total: 0
+main.js:7 current value is: 1
+main.js:6 total: 1
+main.js:7 current value is: 2
+main.js:6 total: 3
+main.js:7 current value is: 3
+main.js:6 total: 6
+main.js:7 current value is: 4
+main.js:6 total: 10
+main.js:7 current value is: 5
 */
