d50218a89c04056030152c3aed668aebd2bc0240
zooprogrammer-cpu <60715599+zooprogrammer-cpu@users.noreply.github.com>
2022-03-27 10:10:14 -0500

    some method

--- focusonforce/main.js
+++ focusonforce/main.js
@@ -1,15 +1,13 @@
-// filter method
+// some method
 var people = [
     {name:"ash", age: 37},
     {name:"brita", age: 33},
     {name:"dug",age:4}
 ]
 
-oldPeople = people.filter(function(currItem,index,actualArr){
+oldPeople = people.some(function(currItem,index,actualArr){
     return currItem.age>30
 })
 
 console.log(oldPeople)
-
-//{name: 'ash', age: 37}
-//{name: 'brita', age: 33}
+// returns true
