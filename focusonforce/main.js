62472953b65021ce91b9c79eb8cafec285fa39ab
zooprogrammer-cpu <60715599+zooprogrammer-cpu@users.noreply.github.com>
2022-03-27 10:31:13 -0500

    sort method for strings

--- focusonforce/main.js
+++ focusonforce/main.js
@@ -1,4 +1,4 @@
-// sort method
+// sort method for strings. mutates original array
 var people = [
     {name:"ash", age: 37},
     {name:"brita", age: 33},
@@ -6,11 +6,12 @@ var people = [
 ]
 
 people.sort(function(a,b){
-    return a.age - b.age
+    return (a.name>b.name) ? 1:(a.name<b.name)? -1:0
 })
 
 console.log(people)
-/*0: {name: 'dug', age: 4}
+/*
+0: {name: 'ash', age: 37}
 1: {name: 'brita', age: 33}
-2: {name: 'ash', age: 37}
+2: {name: 'dug', age: 4}
 */
