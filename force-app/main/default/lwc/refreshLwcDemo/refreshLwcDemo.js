04aaeb219aa2aaf73b0efabae4125313f9d3f934
zooprogrammer-cpu <60715599+zooprogrammer-cpu@users.noreply.github.com>
2022-04-03 20:56:10 -0500

    contacts data table

--- force-app/main/default/lwc/refreshLwcDemo/refreshLwcDemo.js
+++ force-app/main/default/lwc/refreshLwcDemo/refreshLwcDemo.js
@@ -1,11 +1,22 @@
 import { LightningElement,wire } from 'lwc';
 import getContactList from '@salesforce/apex/refreshContactController.getContactList';
+const columns = [
+    { label: 'First Name', fieldName: 'FirstName',editable: true },
+    { label: 'Last Name', fieldName: 'LastName', editable: true },
+    { label: 'Email', fieldName: 'Email', type: 'email' }
+];
 export default class RefreshLwcDemo extends LightningElement {
+    columns = columns;
+    draftValues=[];
     @wire(getContactList)
     contact; 
-    get isContactAvailable(){
-        console.log(JSON.stringify(this.contact))
-        return this.contact && this.contact.data && this.contact.data.length>0? 'Yes':'No'
+    // get isContactAvailable(){
+    //     console.log(JSON.stringify(this.contact))
+    //     return this.contact && this.contact.data && this.contact.data.length>0? 'Yes':'No'
 
+    // }
+
+    handleSave(event){
+        console.log(event.detail.draftValues)
     }
 }
