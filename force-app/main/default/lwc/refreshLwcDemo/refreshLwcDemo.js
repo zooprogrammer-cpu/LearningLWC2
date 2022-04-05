1c652820896d8666010a36fa5bf4ed164ea0ca35
zooprogrammer-cpu <60715599+zooprogrammer-cpu@users.noreply.github.com>
2022-04-05 17:55:04 -0500

    refresh not working

--- force-app/main/default/lwc/refreshLwcDemo/refreshLwcDemo.js
+++ force-app/main/default/lwc/refreshLwcDemo/refreshLwcDemo.js
@@ -1,5 +1,8 @@
 import { LightningElement,wire } from 'lwc';
 import getContactList from '@salesforce/apex/refreshContactController.getContactList';
+import { updateRecord } from 'lightning/uiRecordApi';
+import{ShowToastEvent} from 'lightning/platformShowToastEvent';
+import {refreshApex} from '@salesforce/apex'; 
 const columns = [
     { label: 'First Name', fieldName: 'FirstName',editable: true },
     { label: 'Last Name', fieldName: 'LastName', editable: true },
@@ -7,6 +10,7 @@ const columns = [
 ];
 export default class RefreshLwcDemo extends LightningElement {
     columns = columns;
+    contacts=[];
     draftValues=[];
     @wire(getContactList)
     contact; 
@@ -15,8 +19,46 @@ export default class RefreshLwcDemo extends LightningElement {
     //     return this.contact && this.contact.data && this.contact.data.length>0? 'Yes':'No'
 
     // }
+    // listViewHandler({data, error}){
+    //     if(data){
+    //         console.log(data)
+    //         this.contacts = data.records.records.map(item=>{
+    //             return {
+    //                 "Id": this.getValue(item, 'Id'),
+    //                 "Name": this.getValue(item, 'Name'),
+    //                 "Title": this.getValue(item, 'Title'),
+    //                 "Phone": this.getValue(item, 'Phone'),
+    //                 "Email": this.getValue(item, 'Email')
+    //             }
+    //         })
+    //     }
+    //     if(error){
+    //         console.error(error)
+    //     }
+    // }  
+    
+    // getValue(data, field){
+    //     return data.fields[field].value
+    // }
 
     handleSave(event){
-        console.log(event.detail.draftValues)
+        console.log('These are the draft values:',JSON.stringify(event.detail.draftValues))
+        const recordInputs=event.detail.draftValues
+        
+        console.log('Thes are the recordInputs:',recordInputs)
+
+        updateRecord(recordInputs)
+        this.draftValues=[]
+        return refreshApex(this.contact)
+
+        // const promises = recordInputs.map(recordInput=>updateRecord(recordInput))
+        // Promise.all(promises).then(()=>{
+        //     console.log('COntact updated Successfully')
+        //     this.draftValues=[]
+        //     return refreshApex(this.contact)
+        // }).catch(error=>{
+        //     console.error("Error updating the record", error)
+        // })
+        
     }
 }
