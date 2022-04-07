640a01e18506e1d3b9e1d780f393babe19aff7e2
zooprogrammer-cpu <60715599+zooprogrammer-cpu@users.noreply.github.com>
2022-04-06 22:41:27 -0500

    added isLibLoaded

--- force-app/main/default/lwc/thirdPartyFiles/thirdPartyFiles.js
+++ force-app/main/default/lwc/thirdPartyFiles/thirdPartyFiles.js
@@ -3,16 +3,22 @@ import MOMENT from '@salesforce/resourceUrl/moment'
 import {loadScript} from 'lightning/platformResourceLoader'
 export default class ThirdPartyFiles extends LightningElement {
     currentDate=''
+    isLibLoaded= false
     renderedCallback(){
-        Promise.all([
-            loadScript(this,MOMENT+'/moment/moment.min.js')
-        ])
-        .then(()=>{
-            //success
-            this.setDateOnScreen()
-        }).catch(error=>{
-            console.error(error)
-        })
+        if(this.isLibLoaded){
+            return
+        } else {
+            Promise.all([
+                loadScript(this,MOMENT+'/moment/moment.min.js')
+            ]).then(()=>{
+                //success
+                this.setDateOnScreen()
+            }).catch(error=>{
+                console.error(error)
+            })
+            this.isLibLoaded= true
+        }
+        
     }
     setDateOnScreen(){
         this.currentDate = moment().format('LLLL')
