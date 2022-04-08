26ca30de2d60fa05cef450445a67fdd8a057f770
zooprogrammer-cpu <60715599+zooprogrammer-cpu@users.noreply.github.com>
2022-04-07 22:59:02 -0500

    custom Labels individually

---
+++ force-app/main/default/lwc/customLabel/customLabel.js
@@ -0,0 +1,8 @@
+import { LightningElement } from 'lwc';
+import DESCRIPTION_ONE from '@salesforce/label/c.descriptionOne'
+import DESCRIPTION_TWO from '@salesforce/label/c.descriptionTwo'
+
+export default class CustomLabel extends LightningElement {
+    descriptionOne = DESCRIPTION_ONE
+    descriptionOne = DESCRIPTION_TWO
+}
