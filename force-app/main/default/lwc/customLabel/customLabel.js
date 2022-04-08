e2c5309f7cf722f9945bbb2b4e37d8ecf99c3dc1
zooprogrammer-cpu <60715599+zooprogrammer-cpu@users.noreply.github.com>
2022-04-07 23:02:47 -0500

    custom Label with object

--- force-app/main/default/lwc/customLabel/customLabel.js
+++ force-app/main/default/lwc/customLabel/customLabel.js
@@ -3,6 +3,10 @@ import DESCRIPTION_ONE from '@salesforce/label/c.descriptionOne'
 import DESCRIPTION_TWO from '@salesforce/label/c.descriptionTwo'
 
 export default class CustomLabel extends LightningElement {
-    descriptionOne = DESCRIPTION_ONE
-    descriptionOne = DESCRIPTION_TWO
+   // descriptionOne = DESCRIPTION_ONE
+   // descriptionTwo = DESCRIPTION_TWO
+    LABELS= {
+        descriptionOne:DESCRIPTION_ONE,
+        descriptionTwo:DESCRIPTION_TWO
+    }
 }
