0ec2b6cf76a9aff39cac44a3c693b7feb58fd53f
zooprogrammer-cpu <60715599+zooprogrammer-cpu@users.noreply.github.com>
2022-05-29 12:05:22 -0500

    Bear Child Component Navigation mixin

--- force-app/main/default/lwc/bearList/bearList.js
+++ force-app/main/default/lwc/bearList/bearList.js
@@ -1,50 +1,36 @@
-import { LightningElement,wire } from 'lwc';
-import ursusResources from '@salesforce/resourceUrl/ursus_park';
-/** BearController.getAllBears() Apex method */
-import getAllBears from '@salesforce/apex/BearController.getAllBears';
+import { NavigationMixin } from 'lightning/navigation';
+import { LightningElement, wire } from 'lwc';
+/** BearController.searchBears(searchTerm) Apex method */
 import searchBears from '@salesforce/apex/BearController.searchBears';
-export default class BearList extends LightningElement {
-	//***Imperative Apex */ 
-	// bears;
-	// error;
-	// appResources = {
-	// 	bearSilhouette: `${ursusResources}/img/standing-bear-silhouette.png`,
-	// };
-	// connectedCallback() {
-	// 	this.loadBears();
-	// }
-	// loadBears() {
-	// 	getAllBears()
-	// 		.then(result => {
-	// 			this.bears = result;
-	// 		})
-	// 		.catch(error => {
-	// 			this.error = error;
-	// 		});
-	// }
-
-	//**Wired Apex */
-	searchTerm='';
-	@wire(searchBears,{searchTerm: '$searchTerm'}) bears;
-	appResources = {
-		bearSilhouette: `${ursusResources}/img/standing-bear-silhouette.png`,
-	}; 
-
-	handleSearchTermChange(event){
-		// Debouncing this methid: do not update the reactive property as
-		// long as this function is being called within a delay of 300 ms. 
-		// This is to avoid a very large number of Apex method calls. 
+export default class BearList extends NavigationMixin(LightningElement) {
+	searchTerm = '';
+	@wire(searchBears, {searchTerm: '$searchTerm'})
+	bears;
+	handleSearchTermChange(event) {
+		// Debouncing this method: do not update the reactive property as
+		// long as this function is being called within a delay of 300 ms.
+		// This is to avoid a very large number of Apex method calls.
 		window.clearTimeout(this.delayTimeout);
-		const searchTerm = event.target.value; 
-		this.delayTimeout = setTimeout(()=>{
-			this.searchTerm = searchTerm; 
-		},300); 
+		const searchTerm = event.target.value;
+		// eslint-disable-next-line @lwc/lwc/no-async-operation
+		this.delayTimeout = setTimeout(() => {
+			this.searchTerm = searchTerm;
+		}, 300);
 	}
-
-	get hasResults(){
-		return (this.bears.data.length>0); 
+	get hasResults() {
+		return (this.bears.data.length > 0);
 	}
-
-
-
+	handleBearView(event) {
+		// Get bear record id from bearview event
+		const bearId = event.detail;
+		// Navigate to bear record page
+		this[NavigationMixin.Navigate]({
+			type: 'standard__recordPage',
+			attributes: {
+				recordId: bearId,
+				objectApiName: 'Bear__c',
+				actionName: 'view',
+			},
+		});
+	}
 }
