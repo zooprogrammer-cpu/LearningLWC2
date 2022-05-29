b55559673e01a763c7c4f324a3a6aea566c73f64
zooprogrammer-cpu <60715599+zooprogrammer-cpu@users.noreply.github.com>
2022-05-29 11:30:09 -0500

    Search bears

--- force-app/main/default/lwc/bearList/bearList.js
+++ force-app/main/default/lwc/bearList/bearList.js
@@ -2,6 +2,7 @@ import { LightningElement,wire } from 'lwc';
 import ursusResources from '@salesforce/resourceUrl/ursus_park';
 /** BearController.getAllBears() Apex method */
 import getAllBears from '@salesforce/apex/BearController.getAllBears';
+import searchBears from '@salesforce/apex/BearController.searchBears';
 export default class BearList extends LightningElement {
 	//***Imperative Apex */ 
 	// bears;
@@ -23,9 +24,27 @@ export default class BearList extends LightningElement {
 	// }
 
 	//**Wired Apex */
-	@wire(getAllBears) bears;
+	searchTerm='';
+	@wire(searchBears,{searchTerm: '$searchTerm'}) bears;
 	appResources = {
 		bearSilhouette: `${ursusResources}/img/standing-bear-silhouette.png`,
 	}; 
+
+	handleSearchTermChange(event){
+		// Debouncing this methid: do not update the reactive property as
+		// long as this function is being called within a delay of 300 ms. 
+		// This is to avoid a very large number of Apex method calls. 
+		window.clearTimeout(this.delayTimeout);
+		const searchTerm = event.target.value; 
+		this.delayTimeout = setTimeout(()=>{
+			this.searchTerm = searchTerm; 
+		},300); 
+	}
+
+	get hasResults(){
+		return (this.bears.data.length>0); 
+	}
+
+
 
 }
