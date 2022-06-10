0a97bc6df771078ab10c592a7ad41826d5cd3546
zooprogrammer-cpu <60715599+zooprogrammer-cpu@users.noreply.github.com>
2022-06-10 08:44:50 -0500

    up and down arroww in dataTable

--- force-app/main/default/lwc/dataTableDemo/dataTableDemo.js
+++ force-app/main/default/lwc/dataTableDemo/dataTableDemo.js
@@ -3,7 +3,8 @@ import getAccounts from '@salesforce/apex/tableController.getAccounts';
 const COLUMNS = [
     {label:'Account Name', fieldName:'Name'},
     {label:'Annual Revenue', fieldName:'AnnualRevenue', type:'currency', cellAttributes:{
-        class:{fieldName:'amountColor'}
+        class:{fieldName:'amountColor'},
+        iconName : {fieldName: 'iconName'}, iconPosition:'right'
     }},
     {label:'Industry', fieldName:'Industry'},
     {label:'Phone', fieldName:'Phone'}
@@ -24,7 +25,8 @@ export default class DataTableDemo extends LightningElement {
         if(data){
             this.tableData = data.map(item=>{
                 let amountColor = item.AnnualRevenue < 500000 ? "slds-text-color_error": "slds-text-color_success"
-                return {...item,"amountColor":amountColor}
+                let iconName = item.AnnualRevenue < 500000 ? "utility:down": "utility:up"
+                return {...item,"amountColor":amountColor,"iconName":iconName}
             });
             console.log(this.tableData);
  
