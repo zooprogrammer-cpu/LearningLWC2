ced3e70b9c29f7d9fc970cacb3fdf830bbbcc3f7
zooprogrammer-cpu <60715599+zooprogrammer-cpu@users.noreply.github.com>
2022-06-11 17:52:22 -0500

    external css

--- force-app/main/default/lwc/dataTableDemo/dataTableDemo.js
+++ force-app/main/default/lwc/dataTableDemo/dataTableDemo.js
@@ -1,12 +1,18 @@
 import { LightningElement,wire } from 'lwc';
 import getAccounts from '@salesforce/apex/tableController.getAccounts';
+import {loadStyle} from 'lightning/platformResourceLoader';
+import COLORS from '@salesforce/resourceUrl/colors'; 
 const COLUMNS = [
-    {label:'Account Name', fieldName:'Name'},
+    {label:'Account Name', fieldName:'Name', cellAttributes:{
+        class:{fieldName:'accountColor'}
+    }},
     {label:'Annual Revenue', fieldName:'AnnualRevenue', type:'currency', cellAttributes:{
         class:{fieldName:'amountColor'},
         iconName : {fieldName: 'iconName'}, iconPosition:'right'
     }},
-    {label:'Industry', fieldName:'Industry'},
+    {label:'Industry', fieldName:'Industry', type:'text',cellAttributes:{
+        class:{fieldName:'industryColor'}
+    }},
     {label:'Phone', fieldName:'Phone'}
 ]
 const COLUMNSLESSINDUSTRY = [
@@ -18,6 +24,7 @@ export default class DataTableDemo extends LightningElement {
     tableData
     columns = COLUMNS
     columnsLessIndustry = COLUMNSLESSINDUSTRY
+    isCssLoaded = false; 
 
     showIndustry =true
     @wire(getAccounts) 
@@ -26,7 +33,12 @@ export default class DataTableDemo extends LightningElement {
             this.tableData = data.map(item=>{
                 let amountColor = item.AnnualRevenue < 500000 ? "slds-text-color_error": "slds-text-color_success"
                 let iconName = item.AnnualRevenue < 500000 ? "utility:down": "utility:up"
-                return {...item,"amountColor":amountColor,"iconName":iconName}
+                return {...item,
+                    "amountColor":amountColor,
+                    "iconName":iconName, 
+                    "industryColor":"slds-icon-custom-custom12 slds-text-color_success",
+                    "accountColor": "datatable-orange"
+                }
             });
             console.log(this.tableData);
  
@@ -34,6 +46,19 @@ export default class DataTableDemo extends LightningElement {
         if(error){
             console.error(error)
         }
+    }
+
+    renderedCallback(){
+        // renderedCallback is dengerous since it reloads anytime enything changes.this means the current component.
+        // return immediately if Css is loaded an will not execute the rest of the code.c/accountCreator
+        // if css is not laoded,  set it to true. 
+        if(this,this.isCssLoaded) return
+        this.isCssLoaded = true  
+        loadStyle(this, COLORS).then(()=>{
+            console.log("loaded successfully")
+        }).catch(error=>{
+            console.error("Error in loading colors",error)
+        })
     }
 
 }
