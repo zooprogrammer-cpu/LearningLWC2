72518daaccad35d6f0979017dffc6a8234c11dfc
zooprogrammer-cpu <60715599+zooprogrammer-cpu@users.noreply.github.com>
2022-07-04 12:59:43 -0400

    export and import modules

---
+++ focusonforce/modules/utils.js
@@ -0,0 +1,11 @@
+// Named export
+export const programmingList =['JavaScript', 'Python']
+
+export const helloHandler =()=>{
+    return "Hello Ash"
+}
+
+export const bookDetails={
+    name: "Salesforce for JS",
+    author: "Ash Basnyat"
+}
