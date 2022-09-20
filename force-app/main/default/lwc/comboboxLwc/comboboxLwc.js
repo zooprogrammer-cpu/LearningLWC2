a7975aec60f6f586b7fa25bbfdfcae07bc08e8d3
zooprogrammer-cpu <60715599+zooprogrammer-cpu@users.noreply.github.com>
2022-09-20 10:40:43 -0500

    comboboxvalues transfering

--- force-app/main/default/lwc/comboboxLwc/comboboxLwc.js
+++ force-app/main/default/lwc/comboboxLwc/comboboxLwc.js
@@ -1,9 +1,12 @@
 import { LightningElement, track } from 'lwc';
-import getAccounts from '@salesforce/apex/comboboxClass.getAccounts'; 
+import getAccounts from '@salesforce/apex/comboboxClass.getAccounts';
 
 export default class ComboboxLwc extends LightningElement {
     @track value = '';
-    @track optionsArray =[];
+    @track value2 = '';
+    @track optionsArray = [];
+    @track optionsArray2 = [];
+
 
     get options() {
         // return [
@@ -13,19 +16,41 @@ export default class ComboboxLwc extends LightningElement {
         return this.optionsArray;
     }
 
-    connectedCallback(){
-        getAccounts()
-        .then(result=>{
-            let arr = [];
-            for(let i = 0; i<result.length; i++){
-                arr.push({label: result[i].Name , value: result[i].Id})
-            }
-            this.optionsArray = arr
-        })
+    get options2() {
+        // return [
+        //     { label: 'New', value: 'new value' },
+        //     { label: 'Salesforce', value: 'Sf Value' }
+        // ];
+        return this.optionsArray2;
     }
 
-    handleChanged(event){
+    connectedCallback() {
+        getAccounts()
+            .then(result => {
+                let arr = [];
+                for (let i = 0; i < result.length; i++) {
+                    arr.push({ label: result[i].Name, value: result[i].Id })
+                }
+                this.optionsArray = arr
+            })
+        getAccounts()
+            .then(res => {
+                let arr2 = [];
+                for (let j = 0; j < res.length; j++) {
+                    arr2.push({ label: res[j].Name, value: res[j].Id })
+                }
+                this.optionsArray2 = arr2
+            })
+    }
+
+    handleChanged(event) {
         this.value = event.detail.value
+        this.value2 = this.value
+    }
+
+    handleChanged2(event) {
+        this.value = event.detail.value
+        this.value2 = this.value
     }
 
 
