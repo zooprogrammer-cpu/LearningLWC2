4b0d2ce1e3d8c6636bf23b9fa53883d5d6418778
zooprogrammer-cpu <60715599+zooprogrammer-cpu@users.noreply.github.com>
2023-03-05 22:11:32 -0600

    empAPI showToast cleaned

--- force-app/main/default/lwc/lwcEmpApiNotification/lwcEmpApiNotification.js
+++ force-app/main/default/lwc/lwcEmpApiNotification/lwcEmpApiNotification.js
@@ -1,10 +1,12 @@
 import { LightningElement, api, track } from 'lwc';
 import { subscribe, unsubscribe, onError, setDebugFlag, isEmpEnabled } from 'lightning/empApi';
 import { ShowToastEvent } from "lightning/platformShowToastEvent";
+import currentUserId from '@salesforce/user/Id';
 
 export default class LwcEmpApiNotification extends LightningElement {
     @track message = "";
     @track toastMessage;
+    @api recordId;
     channelName = "/event/empApiEvent__e";
     isSubscribeDisabled = false;
     isUnsubscribeDisabled = !this.isSubscribeDisabled;
@@ -21,30 +23,13 @@ export default class LwcEmpApiNotification extends LightningElement {
         const toastCallback = function (response){
             console.log('message received: ', response.data.payload.message__c);
             self.message = response.data.payload.message__c;
-            //this.toastMessage = response.data.payload.message__c; 
-            //self.toastData = response['data']['payload'];
-            //toastData = self.toastData; 
             console.log('self.message: ', self.message);
             if (self.message){
                 self.showToast("Success",self.message,"success")
             }
         }
-        
-    
- 
-        //this.showToast("Success",this.message,"success")
-        // if (self.message){
-        //     const toastEvent = new ShowToastEvent({
-        //         title : 'Success',
-        //         message: 'yesssss',
-        //         variant: 'success'
-        //     });
-        //     self.dispatchEvent(toastEvent);
-
-        // }
 
 
-        
         // Register error listener
         this.registerErrorListener();
 
@@ -56,45 +41,11 @@ export default class LwcEmpApiNotification extends LightningElement {
                 JSON.stringify(response.channel)
             );
             this.subscription = response;
-            //c/bearLocationthis.toggleSubscribeButton(true);
         });
         onError(error => {
             console.log('Error in Platform Event Toast');
             console.log(error);
         });
-    }
-
-    // Handles subscribe button click
-    // handleSubscribe() {
-    //     // Callback invoked whenever a new event message is received
-    //     const self = this; 
-    //     const messageCallback = function (response) {
-    //         //console.log('New message received: ', JSON.stringify(response.data));
-    //         //this.message = response.data.payload.message__c; 
-    //         console.log('message received: ', response.data.payload.message__c);
-    //         self.message = response.data.payload.message__c;
-    //         this.toastMessage = response.data.payload.message__c; 
-    //         //toastHandler(self.message);
-
-    //         // Response contains the payload of the new message received
-    //     };
-
-    // }
-
-    // Handles unsubscribe button click
-    handleUnsubscribe() {
-        this.toggleSubscribeButton(false);
-
-        // Invoke unsubscribe method of empApi
-        unsubscribe(this.subscription, (response) => {
-            console.log('unsubscribe() response: ', JSON.stringify(response));
-            // Response is true for successful unsubscribe
-        });
-    }
-
-    toggleSubscribeButton(enableSubscribe) {
-        this.isSubscribeDisabled = enableSubscribe;
-        this.isUnsubscribeDisabled = !enableSubscribe;
     }
 
     registerErrorListener() {
@@ -124,6 +75,5 @@ export default class LwcEmpApiNotification extends LightningElement {
             console.log(response);
         });
     }
-
 
 }
