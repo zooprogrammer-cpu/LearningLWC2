1a14bf50a72865b9276ee31284c66dfa2a400687
zooprogrammer-cpu <60715599+zooprogrammer-cpu@users.noreply.github.com>
2023-05-29 15:32:55 -0500

    customMetadata with Helper JS

--- force-app/main/default/lwc/customMetadataLwcGetAll/customMetadataLwcGetAll.js
+++ force-app/main/default/lwc/customMetadataLwcGetAll/customMetadataLwcGetAll.js
@@ -1,6 +1,7 @@
 import { LightningElement, wire, track } from 'lwc';
 import getDiscount from '@salesforce/apex/CustomMetadataService.getDiscount';
 import getAllMetadata from '@salesforce/apex/CustomMetadataService.getAllMetadata';
+import {metadataResults} from 'c/customMetadataHelper';
 
 export default class CustomMetadataLwcGetAll extends LightningElement {
     
@@ -31,9 +32,16 @@ export default class CustomMetadataLwcGetAll extends LightningElement {
 
     handleChange(event) {
         this.selectedInstallemnts = event.detail.value;
-        this.discount = this.dataFormatted[this.selectedInstallemnts].Default_Discount__c;
+        // this.discount = this.dataFormatted[this.selectedInstallemnts].Default_Discount__c;
+        this.discount = metadataResults(this.selectedInstallemnts, this.dataFormatted); 
     
    
     }
 
+    
+
 }
+
+
+
+
