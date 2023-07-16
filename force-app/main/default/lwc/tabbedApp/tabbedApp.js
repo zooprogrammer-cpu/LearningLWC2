82a0003f9c345c765405eced3ad90bd0edc48ee6
zooprogrammer-cpu <60715599+zooprogrammer-cpu@users.noreply.github.com>
2023-07-16 10:46:39 -0500

    Active Tab Working

--- force-app/main/default/lwc/tabbedApp/tabbedApp.js
+++ force-app/main/default/lwc/tabbedApp/tabbedApp.js
@@ -4,6 +4,12 @@ export default class TabbedApp extends LightningElement {
 
     handleOpenTabOne(){
         console.log('Tab One Clicked');
+        let tabOneTitle = this.template.querySelector('[title="Item One"]');
+        tabOneTitle.classList.add('slds-is-active');
+        let tabTwoTitle = this.template.querySelector('[title="Item Two"]');
+        tabTwoTitle.classList.remove('slds-is-active');
+        let tabThreeTitle = this.template.querySelector('[title="Item Three"]');
+        tabThreeTitle.classList.remove('slds-is-active');
         let tabOne = this.template.querySelector('#tab-default-1-200');
         let tabTwo = this.template.querySelector('#tab-default-2-200');
         let tabThree = this.template.querySelector('#tab-default-3-200');
@@ -15,6 +21,12 @@ export default class TabbedApp extends LightningElement {
 
     handleOpenTabTwo(){
         console.log('Tab Two Clicked');
+        let tabTwoTitle = this.template.querySelector('[title="Item Two"]');
+        tabTwoTitle.classList.add('slds-is-active');
+        let tabOneTitle = this.template.querySelector('[title="Item One"]');
+        tabOneTitle.classList.remove('slds-is-active');
+        let tabThreeTitle = this.template.querySelector('[title="Item Three"]');
+        tabThreeTitle.classList.remove('slds-is-active');
         let tabOne = this.template.querySelector('#tab-default-1-200');
         let tabTwo = this.template.querySelector('#tab-default-2-200');
         let tabThree = this.template.querySelector('#tab-default-3-200');
@@ -26,6 +38,12 @@ export default class TabbedApp extends LightningElement {
 
     handleOpenTabThree(){
         console.log('Tab Three Clicked');
+        let tabThreeTitle = this.template.querySelector('[title="Item Three"]');
+        tabThreeTitle.classList.add('slds-is-active');
+        let tabOneTitle = this.template.querySelector('[title="Item One"]');
+        tabOneTitle.classList.remove('slds-is-active');
+        let tabTwoTitle = this.template.querySelector('[title="Item Two"]');
+        tabTwoTitle.classList.remove('slds-is-active');
         let tabOne = this.template.querySelector('#tab-default-1-200');
         let tabTwo = this.template.querySelector('#tab-default-2-200');
         let tabThree = this.template.querySelector('#tab-default-3-200');
