35af5a92c0d9fd1cfe360931c4fef1b7e49d40c3
zooprogrammer-cpu <60715599+zooprogrammer-cpu@users.noreply.github.com>
2023-07-19 14:32:35 -0500

    parent to child with lwc spread

--- force-app/main/default/lwc/p2cParentComponent/p2cParentComponent.js
+++ force-app/main/default/lwc/p2cParentComponent/p2cParentComponent.js
@@ -1,25 +1,10 @@
 import { LightningElement } from 'lwc';
 
 export default class P2cParentComponent extends LightningElement {
-    carouselData=[
-        {
-            src:"https://www.lightningdesignsystem.com/assets/images/carousel/carousel-01.jpg",
-            header:"First Card",
-            description:"First Description"
-        },
-        {
-            src:"https://www.lightningdesignsystem.com/assets/images/carousel/carousel-02.jpg",
-            header:"Second Card",
-            description:"Second Description"
-        },
-        {
-            src:"https://www.lightningdesignsystem.com/assets/images/carousel/carousel-03.jpg",
-            header:"Third Card",
-            description:"Third Description"
-        }
-    ]
+
     //progress bar component
     percentage = 10 //default percentage indicator value is 10 
+    name = 'Ash'
     changeHandler(event){
         this.percentage = event.target.value
 
